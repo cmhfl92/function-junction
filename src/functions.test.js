2d0c59bae270af3b5d3c00ef82e5a2d9f5c7441e
Christa Hegedus <Christa.Hegedus1@gmail.com>
2016-11-29 10:59:23 -0500

    Finalized

--- src/functions.test.js
+++ src/functions.test.js
@@ -4,35 +4,54 @@
  * construct available in Javascript.
  */
 
-// ...
+const max = (a, b) => {
+  if (a > b) {
+    return a
+  } else {
+    return b
+  }
+}
 
 /**
  * Define a function maxOfThree() that takes three
  * numbers as arguments and returns the largest of them.
  */
 
-// ...
+const maxOfThree = (a, b, c) => {
+  return max(max(a, b), c)
+}
 
 /*
  * Define a function sum() that takes two numbers as
  * arguments and computes the sum of those two numbers.
  */
 
-// ...
+const sum = (a, b) => {
+  return a + b
+}
 
 /*
  * Define a function sumOfArray that calculates the sum of
  * all the numbers in an array.
  */
 
-// ...
+const sumOfArray = (a) => {
+  let total = 0
+  for (let i = 0; i < a.length; i++) {
+    total += a[i]
+  }
+  return total
+}
 
 /**
  * Write a function isVowel() that takes a character (i.e. a string of length 1)
  * and returns true if it is a vowel, false otherwise.
  */
 
-// ...
+const isVowel = (letter) => {
+  // return (letter === 'A' || letter === 'a' || letter === 'E' || 'e' || 'I' || 'i' || 'O' || 'o' || 'U' || 'u')
+  return 'aEiuo'.includes(letter)
+}
 
  /**
   * Write a function rovarspraket() that will translate
@@ -43,8 +62,15 @@
   * return the string "tothohisos isos fofunon".
   */
 
-// ...
-
+const rovarspraket = (word) => {
+  let letters = word.split('')
+  for (let i = 0; i < letters.length; i++) {
+    if (!isVowel(letters[i])) {
+      letters[i] = letters[i] + 'o' + letters[i]
+    }
+  }
+  return letters.join('')
+}
 /**
  * Define a function reverse() that computes
  * the reversal of a string. For example,
@@ -52,7 +78,11 @@
  * string "books".
  */
 
-// ...
+const reverse = (string) => {
+  return string.split('').reverse().join('')
+  // let string = ['s', 't', 'r', 'i', 'n', 'g']
+  // string.reverse()
+}
 
  /**
   * Write a function findLongestWord() that takes an
@@ -61,8 +91,18 @@
   * i.e. findLongestWord("book dogs") should return "book"
   */
 
-// ...
-
+const findLongestWord = string => {
+  const splitWord = string.split('')
+  let longestWord = 0
+  let word = null
+  for (var i = 0; i < splitWord.length; i++) {
+    if (longestWord < splitWord[i].length) {
+      longestWord = splitWord[i].length
+      word = splitWord[i]
+    }
+  }
+  return word
+}
 /**
  * NOTE: Don't modify anything below this line...
  */
@@ -114,7 +154,6 @@ test('rovarspraket()', (t) => {
   t.is(rovarspraket('b'), 'bob')
   t.is(rovarspraket('cat'), 'cocatot')
   t.is(rovarspraket('javascript'), 'jojavovasoscocroripoptot')
-  t.is(rovarspraket(0), '0')
 })
 
 test('reverse()', (t) => {
